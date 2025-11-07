d31467bde975551837c50a38328eff6d61399622
RANDRIANASOLO Tafita Fitia <tafitaran05@gmail.com>
2025-11-07 18:50:25 +0100

    add sprint 3

--- src/FrontServlet.java
+++ src/FrontServlet.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.RequestDispatcher;
+import myframework.util.AnnotationScanner;
 
 public class FrontServlet extends HttpServlet {
 
@@ -19,6 +20,13 @@ public class FrontServlet extends HttpServlet {
         if (path == null || path.isEmpty()) {
             path = "/";
         }
+        
+        if ("/".equals(path)) {
+            resp.setContentType("text/html; charset=UTF-8");
+            AnnotationScanner.scanAndDisplay(resp.getWriter());
+            return;
+        }
+        
         boolean resourceExists = getServletContext().getResource(path) != null;
         
         if (resourceExists) {
