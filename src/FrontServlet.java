e491d8fb3d513487fd433bdeedc23466b7b88a0d
RANDRIANASOLO Tafita Fitia <tafitaran05@gmail.com>
2025-11-10 15:49:36 +0300

    add sprint 4 pour l'instant

--- src/FrontServlet.java
+++ src/FrontServlet.java
@@ -1,17 +1,33 @@
 package myframework;
 
 import java.io.IOException;
+import java.util.HashMap;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.RequestDispatcher;
+import java.lang.reflect.Method;
 import myframework.util.AnnotationScanner;
+import myframework.util.Mapping;
 
 public class FrontServlet extends HttpServlet {
+    
+    private HashMap<String, Mapping> urlMappings;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        urlMappings = AnnotationScanner.scanControllers();
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        findResource(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         findResource(req, resp);
     }
 
@@ -21,19 +37,26 @@ public class FrontServlet extends HttpServlet {
             path = "/";
         }
         
-        if ("/".equals(path)) {
-            resp.setContentType("text/html; charset=UTF-8");
-            AnnotationScanner.scanAndDisplay(resp.getWriter());
-            return;
-        }
-        
-        boolean resourceExists = getServletContext().getResource(path) != null;
-        
-        if (resourceExists) {
-            RequestDispatcher defaultDispatcher = getServletContext().getNamedDispatcher("default");
-            defaultDispatcher.forward(req, resp);
-        } else {
-            resp.getWriter().println(path + " non existant");
-        }
+        if(urlMappings.containsKey(path)) {
+                Mapping mapp = urlMappings.get(path);
+                Method method = mapp.getMethod();
+
+                if(method.getReturnType().equals(String.class)) {
+                    try {
+                        Object controllerInstance = mapp.getClazz().getDeclaredConstructor().newInstance();
+                        String result = (String) method.invoke(controllerInstance);
+                        resp.getWriter().println(result);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Error");
+                    }
+                } else {
+                    resp.getWriter().println(path + " existe mais n'est pas supporté pour le moment");
+                }
+            }
+            else {
+            resp.getWriter().println(path + " n'est pas là'");
+            }
     }
+
 }
